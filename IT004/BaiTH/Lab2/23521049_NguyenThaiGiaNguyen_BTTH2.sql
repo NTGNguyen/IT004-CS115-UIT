afe0d9e348ba60f2a9c29ec4ac0cd06c30b8420a
NTGNguyen <23521049@gm.uit.edu.vn>
2024-10-16 10:18:49 +0700

    🩹 Lazy too write Src

--- IT004/BaiTH/Lab2/23521049_NguyenThaiGiaNguyen_BTTH2.sql
+++ IT004/BaiTH/Lab2/23521049_NguyenThaiGiaNguyen_BTTH2.sql
@@ -1,161 +1,108 @@
-﻿Create database PartShipmentDB
-go
-use PartShipmentDB
-go
-Create table Nhacungcap 
-(
-maNCC		varchar(5),
-tenNCC	varchar(20), 
-trangthai 	numeric(2),
-thanhpho	varchar(30),
-Constraint PKNcc primary key (maNCC)
+﻿--III. Ngôn ngữ truy vấn dữ liệu:
+--1. In ra danh sách các sản phẩm (MASP,TENSP) do “Trung Quoc” sản xuất.
+SELECT MASP,TENSP FROM SANPHAM
+WHERE NUOCSX = 'Trung Quoc'
+--2. In ra danh sách các sản phẩm (MASP, TENSP) có đơn vị tính là “cay”, ”quyen”.
+SELECT MASP,TENSP FROM SANPHAM
+WHERE DVT IN ('cay','quyen')
+--3. In ra danh sách các sản phẩm (MASP,TENSP) có mã sản phẩm bắt đầu là “B” và kết
+--thúc là “01”.
+SELECT MASP,TENSP FROM SANPHAM
+WHERE MASP LIKE 'B%01'
+--4. In ra danh sách các sản phẩm (MASP,TENSP) do “Trung Quốc” sản xuất có giá từ 30.000
+--đến 40.000.
+SELECT MASP,TENSP FROM SANPHAM
+WHERE NUOCSX = 'Trung Quoc' AND GIA >= 30000 AND GIA <= 40000
+--5. In ra danh sách các sản phẩm (MASP,TENSP) do “Trung Quoc” hoặc “Thai Lan” sản
+--xuất có giá từ 30.000 đến 40.000.
+SELECT MASP,TENSP FROM SANPHAM
+WHERE NUOCSX IN ('Trung Quoc','Thai Lan') AND GIA >= 30000 AND GIA <= 40000
+--6. In ra các số hóa đơn, trị giá hóa đơn bán ra trong ngày 1/1/2007 và ngày 2/1/2007.
+SELECT SOHD,TRIGIA FROM HOADON
+WHERE NGHD <= '2007-01-02' AND NGHD >= '2007-01-01'
+--7. In ra các số hóa đơn, trị giá hóa đơn trong tháng 1/2007, sắp xếp theo ngày (tăng dần) và
+--trị giá của hóa đơn (giảm dần).
+SELECT SOHD,TRIGIA FROM HOADON
+WHERE MONTH(NGHD) = 1 AND YEAR(NGHD) = 2007 
+ORDER BY NGHD, TRIGIA DESC
+--8. In ra danh sách các khách hàng (MAKH, HOTEN) đã mua hàng trong ngày 1/1/2007.
+SELECT kh.MAKH,HOTEN FROM HOADON hd
+INNER JOIN KHACHHANG kh
+ON hd.MAKH = kh.MAKH
+WHERE NGHD >= '2007-01-01'
+--9. In ra số hóa đơn, trị giá các hóa đơn do nhân viên có tên “Nguyen Van B” lập trong ngày
+--28/10/2006.
+SELECT SOHD,TRIGIA FROM HOADON hd
+INNER JOIN NHANVIEN nv
+ON hd.MANV = nv.MANV
+WHERE HOTEN = 'Nguyen Van B' AND NGHD >= '2006-10-28'
+--10. In ra danh sách các sản phẩm (MASP,TENSP) được khách hàng có tên “Nguyen Van A”
+--mua trong tháng 10/2006.
+SELECT SOHD,TRIGIA FROM HOADON hd
+INNER JOIN NHANVIEN nv
+ON hd.MANV = nv.MANV
+WHERE HOTEN = 'Nguyen Van B' AND MONTH(NGHD) = 10 AND YEAR(NGHD) = 2006
+--11. Tìm các số hóa đơn đã mua sản phẩm có mã số “BB01” hoặc “BB02”.
+SELECT SOHD FROM CTHD
+WHERE MASP = 'BB01'
+UNION
+SELECT SOHD FROM CTHD
+WHERE MASP = 'BB02'
+--12. Tìm các số hóa đơn đã mua sản phẩm có mã số “BB01” hoặc “BB02”, mỗi sản phẩm
+--mua với số lượng từ 10 đến 20.
+SELECT DISTINCT SOHD FROM CTHD
+WHERE MASP IN('BB01','BB02') AND SL>=10 AND SL<=20
+--13. Tìm các số hóa đơn mua cùng lúc 2 sản phẩm có mã số “BB01” và “BB02”, mỗi sản
+--phẩm mua với số lượng từ 10 đến 20.
+SELECT DISTINCT SOHD FROM CTHD
+WHERE MASP ='BB01' AND SL>=10 AND SL<=20
+INTERSECT
+SELECT DISTINCT SOHD FROM CTHD
+WHERE MASP ='BB02' AND SL>=10 AND SL<=20
+--14. In ra danh sách các sản phẩm (MASP,TENSP) do “Trung Quoc” sản xuất hoặc các sản
+--phẩm được bán ra trong ngày 1/1/2007.
+SELECT DISTINCT sp.MASP,TENSP FROM SANPHAM sp
+INNER JOIN CTHD cthd
+ON sp.MASP = cthd.MASP
+INNER JOIN HOADON hd
+ON cthd.SOHD = hd.SOHD
+WHERE NUOCSX = 'Trung Quoc' OR NGHD = '2007-1-1'
+--15. In ra danh sách các sản phẩm (MASP,TENSP) không bán được.
+SELECT MASP,TENSP FROM SANPHAM
+EXCEPT
+SELECT MASP,TENSP 
+FROM SANPHAM
+WHERE EXISTS(
+SELECT 1 FROM CTHD
+WHERE SANPHAM.MASP = CTHD.MASP
 )
-
-
-create table Phutung 
-(
- maPT		varchar(5),
- tenPT	varchar(10),
- mausac	varchar(10),
- khoiluong	float,
- thanhpho	 varchar(30),
- Constraint PKPt Primary Key (maPT) 
+--16. In ra danh sách các sản phẩm (MASP,TENSP) không bán được trong năm 2006.
+SELECT MASP,TENSP FROM SANPHAM
+EXCEPT
+SELECT MASP,TENSP 
+FROM SANPHAM
+WHERE EXISTS(
+SELECT 1 FROM CTHD
+WHERE SANPHAM.MASP = CTHD.MASP
+AND EXISTS (
+SELECT 1 FROM HOADON 
+WHERE HOADON.SOHD = CTHD.SOHD
+AND YEAR(HOADON.NGHD) NOT IN (2006) 
 )
-
-
-Create table Vanchuyen 
-(
-maNCC		varchar(5) ,
-maPT		varchar(5),
-soluong	numeric(5), 
-Constraint PKVc primary key (maNCC,maPT),
-Constraint FKShip1 foreign key (maNCC) references Nhacungcap (maNCC),
-Constraint FKShip2 foreign key (maPT) references Phutung (maPT)
 )
-
-
-insert into Nhacungcap values ('S1','Smith','20','London')
-insert into Nhacungcap values ('S2','Jones','10','Paris')
-insert into Nhacungcap values ('S3','Blake','30','Paris')
-insert into Nhacungcap values ('S4','Clark','20','London')
-insert into Nhacungcap values ('S5','Adams','30','Athens')
-
-
-Insert  into Phutung values  ( 'P1' , 'Nut' , 'Red' , 12.0 , 'London')
-Insert  into Phutung values  ( 'P2' , 'Bolt' , 'Green', 17.0 , 'Paris')
-Insert  into Phutung values  ( 'P3' , 'Screw' , 'Blue', 17.0 , 'Oslo')
-Insert  into Phutung values  ( 'P4' , 'Screw' , 'Red' , 14.0 , 'London')
-Insert  into Phutung values  ( 'P5' , 'Cam' , 'Blue' , 12.0 , 'Paris')
-Insert  into Phutung values  ( 'P6' , 'Cog' , 'Red' , 19.0 , 'London')
-
-
-
-Insert into Vanchuyen values ('S1','P1',300)
-Insert into Vanchuyen values ('S1','P2',200)
-Insert into Vanchuyen values ('S1','P3',400)
-Insert into Vanchuyen values ('S1','P4',200)
-Insert into Vanchuyen values ('S1','P5',100)
-Insert into Vanchuyen values ('S1','P6',100)
-Insert into Vanchuyen values ('S2','P1',300)
-Insert into Vanchuyen values ('S2','P2',400)
-Insert into Vanchuyen values ('S3','P2',200)
-Insert into Vanchuyen values ('S4','P2',200)
-Insert into Vanchuyen values ('S4','P4',300)
-Insert into Vanchuyen values ('S4','P5',400)
-
-select * from Vanchuyen 
-SELECT * FROM Nhacungcap
-SELECT * FROM Phutung
-	--1. Hiển thị thông tin (maNCC, tenNCC, thanhpho) của tất cả nhà cung cấp.
-	SELECT maNCC,tenNCC,thanhpho from Nhacungcap
-	--2. Hiển thị thông tin của tất cả các phụ tùng.
-	SELECT * FROM Phutung
-	--3. Hiển thị thông tin các nhà cung cấp ở thành phố London.
-	SELECT * FROM Nhacungcap
-	WHERE thanhpho = 'London'
-	--4. Hiển thị mã phụ tùng, tên và màu sắc của tất cả các phụ tùng ở thành 
-	--phố Paris.
-	SELECT maPT,tenPT,mausac FROM Phutung
-	WHERE thanhpho = 'Paris'
-	--5. Hiển thị mã phụ tùng, tên, khối lượng của những phụ tùng có khối 
-	--lượng lớn hơn 15.
-	SELECT maPT,tenPT,khoiluong FROM Phutung
-	WHERE khoiluong > 15
-	--6. Tìm những phụ tùng (maPT, tenPt, mausac) có khối lượng lớn hơn 15, 
-	--không phải màu đỏ (red). 
-	SELECT maPT,tenPT,mausac FROM Phutung
-	WHERE NOT (mausac = 'Red' AND khoiluong<=15)
-	--7. Tìm những phụ tùng (maPT, tenPt, mausac) có khối lượng lớn hơn 15, 
-	--màu sắc khác màu đỏ (red) và xanh (green).
-	SELECT maPT,tenPT,mausac FROM Phutung
-	WHERE (khoiluong>15) AND (mausac NOT IN ('Red','Green'))
-	--8. Hiển thị những phụ tùng (maPT, tenPT, khối lượng) có khối lượng lớn 
-	--hơn 15 và nhỏ hơn 20, sắp xếp theo tên phụ tùng.
-	SELECT maPT,tenPT,khoiluong FROM Phutung
-	WHERE (khoiluong>15) AND (khoiluong<20)
-	ORDER BY tenPT
-	--9. Hiển thị những phụ tùng được vận chuyển bởi nhà cung cấp có mã số S1. 
-	--Không hiển thị kết quả trùng. (sử dụng phép kết).
-	SELECT DISTINCT vc.maPT,pt.tenPT,pt.mausac,pt.khoiluong,pt.thanhpho	
-	FROM Vanchuyen vc INNER JOIN
-	Phutung pt ON vc.maPT = pt.maPT
-	WHERE vc.maNCC ='S1'
-	--10. Hiển thị những nhà cung cấp vận chuyển phụ tùng có mã là P1 (sử dụng 
-	--phép kết).
-	SELECT ncc.maNCC,ncc.tenNCC,ncc.trangthai,ncc.thanhpho
-	FROM Nhacungcap ncc INNER JOIN
-	Vanchuyen vc ON ncc.maNCC = vc.maNCC
-	WHERE vc.maPT = 'P1'
-	--11. Hiển thị thông tin nhà cung cấp ở thành phố London và có vận chuyển 
-	--phụ tùng của thành phố London. Không hiển thị kết quả trùng. (Sử dụng 
-	--phép kết)
-	SELECT DISTINCT ncc.maNCC,ncc.tenNCC,ncc.trangthai
-	FROM Nhacungcap ncc
-	INNER JOIN Vanchuyen vc ON vc.maNCC = ncc.maNCC
-	INNER JOIN Phutung pt ON pt.maPT =vc.maPT
-	WHERE ncc.thanhpho = 'London' AND pt.thanhpho = 'London'
-	--12. Lặp lại câu 9 nhưng sử dụng toán tử IN.
-	SELECT DISTINCT vc.maPT,pt.tenPT,pt.mausac,pt.khoiluong,pt.thanhpho	
-	FROM Vanchuyen vc INNER JOIN
-	Phutung pt ON vc.maPT = pt.maPT
-	WHERE vc.maNCC IN('S1')
-	--13. Lặp lại câu 10 nhưng sử dụng toán tử IN
-	SELECT ncc.maNCC,ncc.tenNCC,ncc.trangthai,ncc.thanhpho
-	FROM Nhacungcap ncc INNER JOIN
-	Vanchuyen vc ON ncc.maNCC = vc.maNCC
-	WHERE vc.maPT IN ('P1')
-	--14. Lặp lại câu 9 nhưng sử dụng toán tử EXISTS
-	SELECT *
-	FROM Phutung pt
-	WHERE EXISTS
-	(
-		SELECT 1
-		FROM Vanchuyen vc
-		WHERE pt.maPT = vc.maPT AND vc.maNCC = 'S1'
-	)
-	
-	--15. Lặp lại câu 10 nhưng sử dụng toán tử EXISTS
-	SELECT * 
-	FROM Nhacungcap ncc
-	WHERE EXISTS
-	(
-		SELECT 1
-		FROM Vanchuyen vc
-		WHERE vc.maNCC = ncc.maNCC AND vc.maPT = 'P1'
-	)
-	--16. Lặp lại câu 11 nhưng sử dụng truy vấn con. Sử dụng toán tử IN.
-	SELECT DISTINCT t_1.maNCC,t_1.tenNCC,t_1.trangthai FROM(
-	SELECT ncc.maNCC,ncc.tenNCC,ncc.trangthai,ncc.thanhpho,vc.maPT
-	FROM Nhacungcap ncc INNER JOIN 
-	Vanchuyen vc ON ncc.maNCC = vc.maNCC) t_1
-	INNER JOIN Phutung pt ON pt.maPT = t_1.maPT
-	where t_1.thanhpho IN ('London') AND pt.thanhpho IN ('London')
-	--17. Lặp lại câu 11 nhưng dùng truy vấn con. Sử dụng toán tử EXISTS.
-	SELECT ncc.maNCC,ncc.tenNCC,ncc.trangthai
-	FROM Nhacungcap ncc
-	WHERE EXISTS (
-		SELECT 1 
-		FROM Phutung pt
-		INNER JOIN Vanchuyen vc ON vc.maPT = pt.maPT
-		WHERE pt.thanhpho = 'London'
-	) AND ncc.thanhpho = 'London'
+--17. In ra danh sách các sản phẩm (MASP,TENSP) do “Trung Quoc” sản xuất không bán
+--được trong năm 2006.
+SELECT MASP,TENSP FROM SANPHAM
+WHERE NUOCSX = 'Trung Quoc'
+EXCEPT
+SELECT MASP,TENSP 
+FROM SANPHAM
+WHERE EXISTS(
+SELECT 1 FROM CTHD
+WHERE SANPHAM.MASP = CTHD.MASP
+AND EXISTS (
+SELECT 1 FROM HOADON 
+WHERE HOADON.SOHD = CTHD.SOHD
+AND YEAR(HOADON.NGHD) NOT IN (2006) 
+)
+)
